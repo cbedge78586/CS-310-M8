ec35d02bbfc4db8d7551eb3e464fca57381d7954
Charles Edge <cbedge@my365.bellevue.edu>
2025-10-28 06:29:25 -0500

    Add files via upload

---
+++ M8 Discussion.cpp
@@ -0,0 +1,37 @@
+﻿// Charles Edge
+// CS310-T301
+// M8: Discussion
+
+#include <iostream>
+using namespace std;
+
+// First class, fish. Prints to screen.
+class Fish.f {
+public:
+    string name;
+    void swim() {
+        cout << name << " is swimming in the ocean." << endl;
+    }
+}; 
+
+// Second class, diver. Prints to screen
+class Diver {
+public:
+    string diverName;
+    void dive() {
+        cout << diverName << " is diving in the ocean." << endl;
+    }
+}; 
+
+// Sets name of fish and diver
+int main() {
+    Fish f;
+    f.name = "Tiger Shark";
+    f.swim();
+
+    Diver d;
+        c.diverName = "Brandon";
+    d.dive(); 
+
+    return 0;
+}
